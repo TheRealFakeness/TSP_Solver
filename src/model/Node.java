201b26dd75325669bdf370de9bdf7b56679e1f09
unknown <cgp9923@gmail.com>
2020-04-20 16:56:09 -0500

    Added documentation for the Node class

--- src/model/Node.java
+++ src/model/Node.java
@@ -2,16 +2,38 @@ package model;
 
 public class Node {
 	
-	//Attributes
-	
+	/*
+	 * The identifier of the node
+	 */
 	private int id;
+	
+	/*
+	 * X coordinate of the node in the cartesian plane
+	 */
 	private double xCoord;
+	
+	/*
+	 * Y coordinate of the node in the cartesian plane
+	 */
 	private double yCoord;
+	
+	/*
+	 * The angle of the node in degrees
+	 */
 	private double angle;
+	
+	/*
+	 * True if the Node belongs to the tour
+	 */
 	private boolean onRoute;
 	
-	// Constructor
-	
+	/*
+	 * Creates an instance of the Node class
+	 * 
+	 * @param id The identifier of the node
+	 * @param xCoord X coordinate of the node in the cartesian plane
+	 * @param yCoord Y coordinate of the node in the cartesian plane
+	 */
 	public Node(int id, double xCoord, double yCoord) {
 		this.id = id;
 		this.xCoord = xCoord;
@@ -21,29 +43,51 @@ public class Node {
 	}
 	
 	
-	// Setters && Getters
 	
+	/*
+	 * Returns the id of the node
+	 * @return int Id of the node
+	 */
 	public int getId() {
 		return id;
 	}
-
+	
+	/*
+	 * Returns the X coordinate of the node
+	 * @returns double X coordinate
+	 */
 	public double getxCoord() {
 		return xCoord;
 	}
-
+	
+	/*
+	 * Returns the Y coordinate of the node
+	 * @returns double Y coordinate
+	 */
 	public double getyCoord() {
 		return yCoord;
 	}
 
+	/*
+	 * Returns True if the node belongs to the tour
+	 * @returns boolean True if the node belongs to the tour
+	 */
 	public boolean isOnRoute() {
 		return onRoute;
 	}
 	
-	
+	/*
+	 * Sets the angle of the node in degrees
+	 * @param angle Angle in degrees
+	 */
 	public void setAngle(double angle) {
 		this.angle = angle;
 	}
 	
+	/*
+	 * Returns the angle of the node
+	 * @return double Angle of the node
+	 */
 	public double getAngle() {
 		return angle;
 	}
