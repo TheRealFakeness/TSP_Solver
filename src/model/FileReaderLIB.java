0b2235ac48338ab529756f76480b8f739a3f0f78
BuildTools <cgp9923@gmail.com>
2020-05-11 18:42:06 -0500

    .

--- src/model/FileReaderLIB.java
+++ src/model/FileReaderLIB.java
@@ -2,37 +2,34 @@ package model;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class FileReaderLIB {
-	
-	
+
 	public static void main(String[] args) throws IOException {
-		String file = "";
 		
-	
-			BufferedReader br = new BufferedReader(new FileReader(new File("file")));
-		
-			String line = null;
-			
-			for(int i=0; i<8; i++) {
-				line = br.readLine();
-			}
-			
-			
-			while(line != null) {
-				
-				//split()
-				//trim()
-				
-				String[] input = line.split(",");
-				
-				line = br.readLine();
-			}
+		String file = "data" + File.separator + "file.txt";
+
+		BufferedReader br = new BufferedReader(new FileReader(new File(file)));
+
+		String line = null;
+
+		for (int i = 0; i < 8; i++) {
+			line = br.readLine();
+		}
+
+		while (line != null) {
+
+			// split()
+			// trim()
+			//subString()
+
+			String[] input = line.split(",");
+
+			line = br.readLine();
+		}
 		
 	}
-	
-	
+
 }
